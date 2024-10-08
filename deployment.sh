728fbea9fa0f442645f29403a136e8a51e38c229
Adeel Ijaz <87276133+codeadeel@users.noreply.github.com>
2024-10-08 12:34:08 +0300

    Setup Repo Cloning to Standardize Deployment

--- deployment.sh
+++ deployment.sh
@@ -5,19 +5,19 @@ DEPLOYMENT_DIRECTORY=./services/fnirswebapp
 # Creating directory for deployment
 mkdir -p $DEPLOYMENT_DIRECTORY
 
-# Moving the curernt workspace to target directory
-cd $DEPLOYMENT_DIRECTORY
-
 # Shutdown docker-compose if file already exist & remove existing Docker Image
-if [ -e "./docker-compose.yml" ]; then
+if [ -e "$DEPLOYMENT_DIRECTORY/docker-compose.yml" ]; then
     docker compose --profile deployment down
     docker rmi codeadeel/private:fnirswebapp
 else 
     echo "docker-compose.yml not Found"
 fi 
 
-# Moving the docker-compose.yml to target directory
-mv ../../docker-compose.yml ./
+# Copying the deployment payload to the target directory
+cp ./fNIRS-BloodVitals/docker-compose.yml $DEPLOYMENT_DIRECTORY/
+
+# Moving the curernt workspace to target directory
+cd $DEPLOYMENT_DIRECTORY
 
 # Run docker-compose.yml
 docker compose --profile deployment up -d
